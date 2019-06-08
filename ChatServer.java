bd489e37299940d78298468a61f0d71eb5a3e989
jyspark46 <21800314@handong.edu>
2019-06-08 14:02:12 +0900

    assignment_06/08

--- ChatServer.java
+++ ChatServer.java
@@ -1,3 +1,6 @@
+
+// https://github.com/jyspark46/SimpleChat
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
@@ -46,16 +49,19 @@ class ChatThread extends Thread{
 	public void run(){
 		try{
 			String line = null;
+			String str = null;
 			while((line = br.readLine()) != null){
 				if(line.equals("/quit"))
 					break;
-				if(line.indexOf("/to ") == 0){
+				if((str = checkword(line))!= null){
+					warning(str);
+				}
+				else if(line.equals("/userlist")){
+					senduserlist();
+				}
+				else if(line.indexOf("/to ") == 0){
 					sendmsg(line);
-				}
-				if(line.equals("/userlist")){
-					send_userlist(id);
-				}
-				else
+				}else
 					broadcast(id + " : " + line);
 			}
 		}catch(Exception ex){
@@ -71,6 +77,56 @@ class ChatThread extends Thread{
 			}catch(Exception ex){}
 		}
 	} // run
+    
+    // 기능 1번
+    // 1. run()에서 읽은 값이 /userlist 이면 send_userlist()를 호출
+    // 2. keyset에 hm의 key를 불러옴
+    // 3. hm의 size도 불러옴
+    // 4. userlist 정보를 보낼 대상 설정
+    // 5. pw 정의 후 keyset 출력 & 사용자 수도 출력
+	private void senduserlist(){
+		int j = 1;
+		PrintWriter pw = null;
+		Object obj = null;
+		Iterator<String> iter = null;
+		synchronized(hm){
+			iter = hm.keySet().iterator();
+			obj = hm.get(id);
+		}
+		if(obj != null){
+				pw = (PrintWriter)obj;
+		}
+		pw.println("<User list>");
+		while(iter.hasNext()){
+				String list = (String)iter.next();
+				pw.println(j+". "+list);
+				j++;
+		}
+		j--;
+		pw.println("Total : "+j+".");
+		pw.flush();
+	}
+    
+    // 기능 3번
+    // 1. 만약 입력받은 문장에 word[] 내의 단어가 포함되어있으면 그 단어를 리턴 (checkword 함수)
+	public String checkword(String msg){
+		int b = 1;
+		String[] word ={"shit","bitch","damn","idiot","fool"};
+		for(int i=0;i<word.length;i++){
+			if(msg.contains(word[i]))
+				return word[i];
+		}
+		return null;
+	}
+    // 2. 리턴받은 글자를 이용해 경고 메시지 출력
+	public void warning(String msg){
+		Object obj = hm.get(id);
+		if(obj != null){
+				PrintWriter pw = (PrintWriter)obj;
+				pw.println("Don't use "+ msg);
+				pw.flush();
+		} // if
+	}
 	public void sendmsg(String msg){
 		int start = msg.indexOf(" ") +1;
 		int end = msg.indexOf(" ", start);
@@ -85,27 +141,32 @@ class ChatThread extends Thread{
 			} // if
 		}
 	} // sendmsg
-    	// 기능 2번
-    	// iter.next() 의 값이 hm.get(id) 의 값과 다르면 즉, 본인이 아니면
-    	// broadcast 실행 (값이 같으면, 즉 본인이면 실행 x)
+    
+    // 기능 2번
+    // 1. iter.next() 의 값이 hm.get(id) 의 값과 다르면 즉, 본인이 아니면
+    // 2. broadcast 실행 (값이 같으면, 즉 본인이면 실행 x)
 	public void broadcast(String msg){
 		synchronized(hm){
 			Collection collection = hm.values();
 			Iterator iter = collection.iterator();
 			while(iter.hasNext()){
 				PrintWriter pw = (PrintWriter)iter.next();
+				PrintWriter pw2 = (PrintWriter)hm.get(id);
+				if(pw==pw2) continue;
 				pw.println(msg);
 				pw.flush();
 			}
 		}
 	} // broadcast
+}
+
   	// 기능 1번
   	// 1. run()에서 읽은 값이 /userlist 이면 send_userlist()를 호출
   	// 2. keyset에 hm의 key를 불러옴
   	// 3. hm의 size도 불러옴
   	// 4. userlist 정보를 보낼 대상 설정
   	// 5. pw 정의 후 keyset 출력 & 사용자 수도 출력
-  	public void send_userlist(String id){
+  	/*public void send_userlist(String id){
     		Set keyset = hm.keySet();
     		int size = hm.size();
 		Object obj = hm.get(id);
@@ -114,6 +175,5 @@ class ChatThread extends Thread{
 		pw.flush();
 		pw.println("user count: "+ size);
 		pw.flush();
-  	}
-}
+  	}*/
 
